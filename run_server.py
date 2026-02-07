2adf75a74f545e90e546cf09d087a638ecc225d5
Siddharth Radhakrishnan <siddharth.radh@gmail.com>
2026-02-07 05:10:36 -0500

    added basic python static server

---
+++ run_server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import send_from_directory
+
+
+app = Flask(__name__, static_url_path='/', static_folder='sisyphus/')
+
+@app.route("/")
+def index():
+    return send_from_directory(app.static_folder, 'game.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
